a69a694285ef4978c5f915d3d33915bf1e6c7736
Mikael Gueck <gumi@iki.fi>
2016-09-25 18:56:19 +0300

    Thymeleaf.

--- src/main/kotlin/io/mikael/talent/app.kt
+++ src/main/kotlin/io/mikael/talent/app.kt
@@ -1,72 +1,33 @@
 package io.mikael.talent
 
-import org.springframework.beans.factory.BeanFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.oauth2.client.OAuth2RestTemplate
-import org.springframework.security.oauth2.provider.OAuth2Authentication
-import org.springframework.security.web.util.matcher.RegexRequestMatcher
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMethod.GET
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.servlet.ModelAndView
-import java.security.Principal
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable as path
-import org.springframework.web.bind.annotation.RequestMapping as http
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
 }
 
 @SpringBootApplication
-@EnableOAuth2Sso
-open class Application : WebSecurityConfigurerAdapter() {
-
-    @Autowired
-    lateinit var myBeanFactory: BeanFactory
-
-    override fun configure(http: HttpSecurity) {
-
-        http.authorizeRequests()
-            .regexMatchers("/", "/favicon.ico", "/webjars.*", "/static.*")
-                .permitAll()
-            .anyRequest()
-                .authenticated()
-            .and()
-            .logout()
-                .logoutRequestMatcher(RegexRequestMatcher("/logout", "GET"))
-                .logoutSuccessUrl("/")
-
-        YammerSsoSecurityConfigurer(this.myBeanFactory).configure(http)
-    }
-
-    @Autowired
-    open fun customizeOAuth2RestTemplate(restTemplate: OAuth2RestTemplate) {
-        restTemplate.setAccessTokenProvider(YammerAuthorizationCodeAccessTokenProvider())
-    }
+open class Application {
 
 }
 
 @Controller
 open class WebUserInterface {
 
-    @http("/", method = arrayOf(GET))
-    fun whatsnew(auth: OAuth2Authentication?) = ModelAndView("index", "auth", auth)
+    @GetMapping("/")
+    fun whatsnew() = "index"
 
-    @http("/project/{id}", method = arrayOf(GET))
+    @GetMapping("/project/{id}")
     fun project(@path id: Int) = ModelAndView("project", "id", id)
 
-    @http("/{username}", method = arrayOf(GET))
-    fun person(auth: OAuth2Authentication, @path username: String) =
-        ModelAndView("person", mapOf("auth" to auth, "username" to username))
-
-    @http(value = "/user", method = arrayOf(GET))
-    @ResponseBody
-    fun user(principal: Principal) = principal
+    @GetMapping("/{username}")
+    fun person(@path username: String) =
+        ModelAndView("person", mapOf("username" to username))
 
 }
 
