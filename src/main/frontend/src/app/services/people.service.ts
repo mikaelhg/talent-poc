74e9f885fee8a1fa41a40503b6960ea75957e055
Mikael Gueck <gumi@iki.fi>
2017-01-28 06:38:28 +0200

    People.

--- src/main/frontend/src/app/services/people.service.ts
+++ src/main/frontend/src/app/services/people.service.ts
@@ -3,33 +3,71 @@ import {Observable} from "rxjs";
 
 export class Person {
 
-  public name: string = "Sauli Niinistö";
+  public name: string;
 
-  public title: string = "President of Finland";
+  public title: string;
 
-  public description: string =
-    `A lawyer by education, Niinistö was Minister of Finance from 1996 to 2003 
-    and the National Coalition Party (NCP) candidate in the 2006 presidential election. 
-    He served as the Speaker of the Parliament of Finland from 2007 to 2011 and has been 
-    the Honorary President of the European People's Party since 2002.`;
+  public description: string;
 
-  public locations: string[] = ['Helsinki'];
+  public locations: string[];
 
-  public roles: string[] = ['Architect', 'Developer'];
+  public roles: string[];
 
-  public skills: string[] = ['Java', 'Cloud', 'Mobile', 'Full Stack'];
+  public skills: string[];
 
-  public interests: string[] = ['IoT', 'Kotlin'];
+  public interests: string[];
 
-  public picture: string = 'https://pbs.twimg.com/profile_images/673420359874428928/EZztR797.jpg';
+  public picture: string;
 
 }
 
 @Injectable()
 export class PeopleService {
 
+  private data = {
+    1: {
+      name: 'Donald Duck',
+      title: 'Technical Architect',
+      description: `Donald Duck is a cartoon character created in 1934 at Walt Disney Productions.
+     Donald is an anthropomorphic white duck with a yellow-orange bill, legs, and feet.
+      He typically wears a sailor shirt and cap with a bow tie.
+       Donald is most famous for his semi-intelligible speech and his mischievous and temperamental personality.`,
+      locations: ['Helsinki'],
+      roles: ['Architect', 'Developer'],
+      skills: ['Java', 'Cloud', 'Mobile', 'Full Stack'],
+      interests: ['IoT', 'Kotlin'],
+      picture: 'http://wondersofdisney.webs.com/pals/donald/donangryface.png'
+    },
+    2: {
+      name: 'Mickey Mouse',
+      title: 'Interaction Designer',
+      description: ``,
+      locations: ['Helsinki'],
+      roles: ['Designer', 'Frontend Developer'],
+      skills: ['Angular 2', 'React.JS', 'Mobile', 'Full Stack'],
+      interests: ['Mobile', 'TypeScript'],
+      picture: 'http://wondersofdisney.webs.com/pals/donald/donangryface.png'
+    },
+    3: {
+      name: 'Scrooge McDuck',
+      title: 'Project Manager',
+      description: ``,
+      locations: ['Helsinki'],
+      roles: ['Project Manager', 'Scrum Master'],
+      skills: ['Project Management', 'Scrum', 'Kanban'],
+      interests: ['Prince2', 'Princess3'],
+      picture: 'http://wondersofdisney.webs.com/pals/donald/donangryface.png'
+    },
+  };
+
   public getPerson(id: number): Observable<Person> {
-    return Observable.of(new Person());
+    return Observable.of(this.data[id] as Person);
   }
+
+  /*
+  public getPeople(): Observable<Person[]> {
+    return Observable.of(this.data as Person[]);
+  }
+  */
 
 }
